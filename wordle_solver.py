03db2ab5147aa610e23a46c68a1c5a56af5dd717
David Link <dlink@Davids-MBP.home>
2022-03-04 22:16:19 -0500

    Now working somewhat

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,9 +1,7 @@
 
-from pprint import pprint
-
-ALL_WORDS = 'all_words'
-
+ALL_WORDS_FILE = 'all_words'
 LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+DEBUG = 1
 
 class Solver():
 
@@ -11,32 +9,135 @@ class Solver():
         self.scored_words = {}
         self.scores = []
         self.num_guesses = 0
+        self.score_ind = -1
+
+        self.clues = {'matches': [],
+                      'hits': [],
+                      'misses': []}
+
         self.initializeData()
 
     def run(self):
-        for i in range(0, 10):
-            print(self.scores[i], self.scored_words[self.scores[i]])
-        print()
+        # show
+        #for score in self.scores:
+        #    print(score, self.scored_words[score])
 
-        quess = self.makeGuess()
-        print(quess)
-        
-    def makeGuess(self):
-        ind = 0
+        guesses = []
         done = 0
         while not done:
-            score = self.scores[ind]
-            break_ = 0
-            for word in self.scored_words[score]:
-                guess = word
-                print(done, score, guess, len(set(guess)))
-                if self.num_guesses != 0 or len(set(guess)) == 5:
-                    done = 1
-                    break_ = 1
-            if break_:
+            guess = self.makeGuess()
+            guesses.append(guess)
+            print('Guess:', guess)
+            while 1:
+                response = input('Correct (C)? or new clues? ')
+                if(self.responseValid(response)):
+                   break
+            if response.lower() == 'c':
+                print('Solved!')
+                done = 1
                 break
-            ind += 1
+            self.updateClues(guess, response)
+        print(guess)
+
+    def makeGuess(self):
+        found = 0
+        while not found:
+            self.score_ind += 1
+            score = self.scores[self.score_ind]
+            for guess in self.scored_words[score]:
+                if DEBUG:
+                    print(score, guess, len(set(guess)))
+
+                # eliminations
+
+                # repeat letters on first guess
+                if self.num_guesses == 0 and len(set(guess)) != 5:
+                    if DEBUG:
+                        print('   repeat letters on first guess')
+                    break
+
+                # must use letter matches
+                mismatch = 0
+                for match in self.clues['matches']:
+                    m = match[0]
+                    p = int(match[1])-1
+                    if guess[p] != m:
+                        mismatch = 1
+                        if DEBUG:
+                            print('   must use letter matches')
+                        break
+                if mismatch:
+                    break
+
+                # must use letter hits
+                non_hit = 0
+                hit_same_spot = 0
+                for hit in self.clues['hits']:
+                    h = hit[0]
+                    p = int(hit[1])-1
+                    if guess[p] == h:
+                        hit_same_spot = 1
+                        if DEBUG:
+                            print('   hit same spot')
+                        break
+                    frag = guess[0:p] + guess[p+1:]
+                    if h not in frag:
+                        non_hit = 1
+                        if DEBUG:
+                            print('   has non hit letter')
+                        break
+                if hit_same_spot or non_hit:
+                    break
+
+                # do not use letter misses
+                has_misses = 0
+                for l in guess:
+                    if l in self.clues['misses']:
+                        has_misses = 1
+                        if DEBUG:
+                            print('   has letter misses')
+                        break
+                if has_misses:
+                    break
+
+                # guess looks good:
+                found = 1
+                break
+
+        self.num_guesses += 1
         return guess
+
+    def responseValid(self, response):
+        if response.lower() == 'c':
+            return 1
+
+        if len(response) != 5:
+            return 0
+
+        # only contains 0, 1, or 2
+        for c in response:
+            if c not in ['0','1','2']:
+                return 0
+        return 1
+
+    def updateClues(self, guess, response):
+        for i, c in enumerate(response):
+            if c == '2':
+                self.clues['matches'].append(guess[i] + str(i+1))
+            elif c == '1':
+                self.clues['hits'].append(guess[i] + str(i+1))
+
+        # separate loop after we've collected all matches
+        for i, c in enumerate(response):
+            if c == '0':
+                match_letters = [a[0] for a in self.clues['matches']]
+                hit_letters = [a[0] for a in self.clues['hits']]
+                if guess[i] not in match_letters and\
+                   guess[i] not in hit_letters:
+                    self.clues['misses'].append(guess[i])
+
+        print('clues:', self.clues)
+        pause = input('pause.')
 
     def initializeData(self):
         words = self.loadWords()
@@ -50,7 +151,7 @@ class Solver():
 
     def loadWords(self):
         words = []
-        for word in open(ALL_WORDS, 'r').readlines():
+        for word in open(ALL_WORDS_FILE, 'r').readlines():
             word = word.strip()
             words.append(word)
         return words
@@ -74,8 +175,6 @@ class Solver():
                 scores[score] = []
             scores[score].append(word)
         return scores
-    
+
 if __name__ == '__main__':
     Solver().run()
-
-
